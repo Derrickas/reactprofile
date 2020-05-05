ebec111b0068b35d5609975e37b9d4c6e160b8dc
Derrickas <Derrickas@outlook.com>
2020-05-05 16:40:19 -0500

    update layout'

---
+++ reactproj/src/components/Aboutme.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+class About extends Component {
+  render() {
+    return(
+      <div><h1>About Page:</h1></div>
+      <h5>lorem</h5>
+    )
+  }
+}
+
+export default About;
---
+++ reactproj/src/components/Landingpage.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
+
+
+class Landing extends Component {
+  render() {
+    return(
+      <div style={{width: '100%', margin: 'auto'}}>
+        <Grid className="landing-grid">
+          <Cell col={12}>
+         
+          <img></img>
+          <div></div>
+
+            <div className="banner-text">
+              <h1>Full Stack Web Developer</h1>
+
+            <hr/>
+
+          <p>HTML/CSS | Bootstrap | JavaScript | React | NodeJS | Express | MongoDB</p>
+
+        <div className="social-links">
+
+          {/* LinkedIn */}
+          <a href="https://www.linkedin.com/in/derrick-schupp/" rel="noopener noreferrer" target="_blank">
+            <i className="fa fa-linkedin-square" aria-hidden="true" />
+          </a>
+
+          {/* Github */}
+          <a href="https://github.com/Derrickas" rel="noopener noreferrer" target="_blank">
+            <i className="fa fa-github-square" aria-hidden="true" />
+          </a>
+
+          {/* Freecodecamp */}
+          <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+            <i className="fa fa-free-code-camp" aria-hidden="true" />
+          </a>
+
+        </div>
+            </div>
+          </Cell>
+        </Grid>
+      </div>
+    )
+  }
+}
+
+export default Landing;
